2f1280e73d14109bafa12e4600b4e426f0c3c563
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-05 14:57:01 +0530

    Navigation on Click | Navigating programmatically

--- my-app/src/component/Home.js
+++ my-app/src/component/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 export default function Home() {
+  const navigate = useNavigate()
+
+  const navToPage=(url)=>{
+    navigate(url)
+    
+  }
   return (
     <div>
      <h1>Homepage</h1>
      <p>This is our awesome App</p>
      <p>and here i am learning react router dom </p>
      <Link to="/about">Go to About Page</Link>
+     <br />
+     <button onClick={()=> navToPage('/about')}>Go to About page</button>
+     <br />
+     <button onClick={()=> navToPage('/filter')}>Go to Filter page</button>
     </div>
   )
 }
